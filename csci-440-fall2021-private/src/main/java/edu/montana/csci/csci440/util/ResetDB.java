7a4c0a664e2ba5c24d14dcc781699b95960042e8
stuartsmith1 <stusmith112@gmail.com>
2021-08-25 14:22:20 -0600

    initial commit

--- csci-440-fall2021-private/src/main/java/edu/montana/csci/csci440/util/ResetDB.java
+++
@@ -1,9 +0,0 @@
-package edu.montana.csci.csci440.util;
-
-import java.io.IOException;
-
-public class ResetDB {
-    public static void main(String[] args) throws IOException {
-        DB.reset();
-    }
-}
